ce99837e217090d1d57c2072cc6011de7cc85d3a
danyramirezg <danyramirezg@gmail.com>
2019-09-25 19:24:26 +0000

    Modification main file

--- main.c
+++ main.c
@@ -142,73 +142,9 @@ void swap(stack_t **top, unsigned int line)
 	if (c)
 		c->prev = a;
 }
-void add(stack_t **top, unsigned int line)
-{
-	stack_t *new_top;
 
-	if (*top == NULL || (*top)->next == NULL)
-		add_err(line);
-
-	new_top = (*top)->next;
-	new_top->n += (*top)->n;
-	pop(top, line);
-}
 void nop(stack_t **top, unsigned int line)
 {
 	(void)top;
 	(void)line;
-}
-void sub(stack_t **top, unsigned int line)
-{
-	stack_t *new_top;
-
-	if (*top == NULL || (*top)->next == NULL)
-		sub_err(line);
-
-	new_top = (*top)->next;
-	new_top->n -= (*top)->n;
-	pop(top, line);
-}
-void div1(stack_t **top, unsigned int line)
-{
-	stack_t *new_top;
-	
-	if (*top == NULL || (*top)->next == NULL) 
-		div_err1(line);	
-	
-	if ((*top)->n == 0)
-		div_err2(line);
-
-	
-	new_top = (*top)->next;
-	new_top->n = (new_top->n) / (*top)->n;
-	pop(top, line);	
-}
-
-void mul(stack_t **top, unsigned int line)
-{
-	stack_t *new_top;
-	
-	if (*top == NULL || (*top)->next == NULL)
-		mul_err(line);
-
-	new_top = (*top)->next;
-	new_top->n *= (*top)->n;
-	pop(top, line);
-	
-}
-void mod(stack_t **top, unsigned int line)
-{
-	stack_t *new_top;
-	
-	if (*top == NULL || (*top)->next ==  NULL)
-		mod_err(line);
-	
-	if ((*top)->n == 0)
-		div_err2(line);
-
-	new_top = (*top)->next;
-	new_top->n = (new_top->n) % (*top)->n;
-	pop(top, line);
-		
 }
