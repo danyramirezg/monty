6fc3a7eb2501897cc5fe1a805c16dbf7afe6a8e4
danyramirezg <danyramirezg@gmail.com>
2019-09-25 18:31:03 +0000

    Correction Betty style

--- error_handler2.c
+++ error_handler2.c
@@ -1,20 +1,46 @@
 #include "monty.h"
 
+/**
+ * pop_err - Handle error for deletion of an empty stack
+ * @line: The function´s parameter
+ * Return: void
+ */
+
 void pop_err(unsigned int line)
 {
 	fprintf(stderr, "L%u: can't pop an empty stack\n", line);
 	exit(EXIT_FAILURE);
 }
+
+/**
+ * swap_err - Error when can´t swap.
+ * @line: The function´s parameter
+ * Return: void
+ */
+
 void swap_err(unsigned int line)
 {
 	fprintf(stderr, "L%u: can't swap, stack too short\n", line);
 	exit(EXIT_FAILURE);
 }
+/**
+ * add_err - Error when can´t add.
+ * @line: The function´s parameter
+ * Return: void
+ */
+
 void add_err(unsigned int line)
 {
 	fprintf(stderr, "L%u: can't add, stack too short\n", line);
 	exit(EXIT_FAILURE);
 }
+
+/**
+ * sub_err - Error when can´t do subtraction
+ * @line: The function´s parameter
+ * Return: void
+ */
+
 void sub_err(unsigned int line)
 {
 	fprintf(stderr, "L%u: can't sub, stack too short\n", line);
