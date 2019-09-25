d5649727cd4e30e17f60ea5e5103beb311514140
danyramirezg <danyramirezg@gmail.com>
2019-09-25 22:20:14 +0000

    Fix file

--- error_handler3.c
+++ error_handler3.c
@@ -8,7 +8,7 @@
 
 void div_err1(unsigned int line)
 {
-	fprintf(stderr, "L%u: can´t div, stack too short\n", line);
+	fprintf(stderr, "L%u: can't div, stack too short\n", line);
 	exit(EXIT_FAILURE);
 }
 
@@ -32,7 +32,7 @@ void div_err2(unsigned int line)
 
 void mul_err(unsigned int line)
 {
-	fprintf(stderr, "L%u: can´t mul, stack too short\n", line);
+	fprintf(stderr, "L%u: can't mul, stack too short\n", line);
 	exit(EXIT_FAILURE);
 }
 
